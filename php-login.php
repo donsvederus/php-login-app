38ea41f56ce27d8c80efa3e37b7d0c8a4fc28c94
donsvederus <svederus@gmail.com>
2020-08-18 21:24:20 -0600

    Initial commit

---
+++ php-login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dons PHP Login App Example</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+
+    <div class="col-xs-6">
+    
+        <form action="php-formcreate.php" method="post">
+        
+            <div class="form-group">
+
+                <label for="username">Username</label>
+                <input type="text" name="username" class="form-control">
+
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            
+            </div>
+
+            <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+        
+        </form>
+    
+    </div>
+
+</div>   
+</body>
+</html>
