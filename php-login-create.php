2216f8314e045fa827c9c0e3f16e199a9a84dfaa
donsvederus <svederus@gmail.com>
2020-08-18 22:31:40 -0600

    0.45 Creating Records into the database with PHP

--- php-login-create.php
+++ php-login-create.php
@@ -3,17 +3,31 @@
 // isset checks if the form was submitted
 if(isset($_POST['submit'])) {
 
-// assigning the form names into variables
-$username = $_POST['username'];
-$password = $_POST['password'];
+    // assigning the form names into variables
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-// This connects to the database, Note: create a database with ID, username, password
-$connection = mysqli_connect('localhost', 'root', '', 'loginapp');
-    if(!$connection) {
-        die("Database connection failed");
+    // This connects to the database, Note: create a database with ID, username, password
+    $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+        if(!$connection) {
+            die("Database connection failed");
+        }
+
+
+    // this inserts the data into the database, and we assign to a variable, so we can check it
+    $query = "INSERT INTO users(username, password) ";  // these are the columns for the db
+    $query .= "VALUES ('$username', '$password')"; // the .= concatenate to the previous line
+
+    // use a prebuilt function,  created a variable for checking
+    $result = mysqli_query($connection, $query);
+
+    // checks and kills the process if things dont work out
+    if(!$result) {
+        die('Query FAILED' . mysqli_error());
+    } else {
+        echo "Data submitted";
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +45,7 @@ $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
     <div class="col-xs-6">
 
         <!-- Login Form Start -->
-        <form action="php-login.php" method="post">
+        <form action="php-login-create.php" method="post">
         
             <div class="form-group">
 
